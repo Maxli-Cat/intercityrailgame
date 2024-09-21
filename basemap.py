2247ceb79aef9ac771ab51b1ccdad6b66c58807a
Sophie <maxlicatenby@gmail.com>
2024-09-20 20:10:23 -0400

    Got map working

---
+++ basemap.py
@@ -0,0 +1,105 @@
+import xyzservices.providers as xyz
+import math
+import urllib.request
+import os
+import time
+from tqdm import tqdm, trange
+
+def sec(x):
+    return 1/math.cos(x)
+
+def arsinh(x):
+    return math.log(x + (x**2 + 1)**0.5)
+
+
+def get_tile_cords(zoom, lat, lon):
+    n = 2 ** zoom
+    xtile = n * ((lon + 180) / 360)
+    lat = math.radians(lat)
+    ytile = n * (1 - (math.log(math.tan(lat) + sec(lat)) / math.pi)) /2
+
+    return (int(xtile), int(ytile))
+
+def get_tile_corner(zoom, x, y):
+    n = 2 ** zoom
+    lon_deg = x / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * y / n)))
+    lat_deg = math.degrees(lat_rad)
+
+    return (lat_deg, lon_deg)
+
+def download_tile(z, x, y, basepath="tiles"):
+    url = f"https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+    #print(url)
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Sophie Johnson Intercity Rail Game 0.0.1')]
+    urllib.request.install_opener(opener)
+    print(url)
+    urllib.request.urlretrieve(url, f"{basepath}\\{z}_{x}_{y}.png")
+    time.sleep(0.1)
+
+def get_tile(z, x, y, basepath="tiles"):
+    filename = f"{basepath}\\{z}_{x}_{y}.png"
+    if not os.path.exists(filename):
+        download_tile(z, x, y, basepath)
+        return
+    filetime = os.path.getmtime(filename)
+    age = time.time() - filetime
+    if age > 604800:
+        download_tile(z, x, y, basepath)
+
+def get_tiles(startlat, startlon, endlat, endlon, zoom, basepath="tiles"):
+    starttile = get_tile_cords(zoom, startlat, startlon)
+    endtile = get_tile_cords(zoom, endlat, endlon)
+    print(starttile, endtile)
+
+    for x in trange(starttile[0], endtile[0]+1):
+        for y in range(starttile[1], endtile[1]+1):
+            #print(x, y)
+            get_tile(zoom, x, y, basepath)
+
+    return (starttile[0], endtile[0]+1), (starttile[1], endtile[1]+1)
+
+def get_count_tiles(start, xcount, ycount, zoom, basepath="tiles"):
+    starttile = get_tile_cords(zoom, *start)
+
+    for x in range(starttile[0], starttile[0]+xcount):
+        for y in range(starttile[1], starttile[1]+ycount):
+            get_tile(zoom, x, y, basepath)
+
+    return (starttile[0], starttile[0] + xcount), (starttile[1], starttile[1] + ycount)
+
+def point_in_range(point, start, end):
+    point = point - start
+    end = end - start
+    return point / end
+
+def get_offset(zoom, lat, lon):
+    on = get_tile_cords(zoom, lat, lon)
+    off = [i+1 for i in on]
+    topleft = get_tile_corner(zoom, *on)
+    bottomright = get_tile_corner(zoom, *off)
+    #print(topleft, (lat,lon), bottomright)
+    x = point_in_range(lat, topleft[0], bottomright[0])
+    y = point_in_range(lon, topleft[1], bottomright[1])
+    return (x * 255, y * 255)
+
+
+
+
+
+if __name__ == '__main__':
+    for zoom in range(0, 19):
+        coords = get_tile_cords(zoom, 43.052976, -71.073759)
+        #print(f"https://tile.openstreetmap.org/{zoom}/{coords[0]}/{coords[1]}.png")
+        #corner = get_tile_corner(zoom, *coords)
+        #print(*corner)
+        #get_tile(zoom, coords[0], coords[1])
+
+    startcorner = (43.0733739,-71.1216099)
+    endcorner = (43.0050499,-71.0245459)
+
+    get_tiles(startcorner[0], startcorner[1], endcorner[0], endcorner[1], 14)
+    #coords = get_tile_cords(16, 43.044308, -71.077123)
+    #get_tile(16, coords[0], coords[1])
+    #get_offset(16, 43.044308, -71.077123)
--- map.py
+++ map.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import time
+import basemap
 
 pygame.init()
 pygame.font.init()
@@ -12,11 +13,83 @@ SIZE = WIDTH, LENGTH = (900, 600)
 
 screen = pygame.display.set_mode(SIZE, FLAGS)
 
+startcorner = [***REMOVED***]
+
+zoom_factor = 13
+move_factor = 40 / (2 ** zoom_factor)
+
+tile_surfaces = {}
+cache_hits = 0
+cache_misses = 0
+
+def get_images_cache(zoom, x, y):
+    global cache_hits, cache_misses
+    key = f"{zoom}_{x}_{y}"
+    if key in tile_surfaces.keys():
+        cache_hits += 1
+        return tile_surfaces[key]
+    cache_misses += 1
+    filename = f"tiles\\{zoom}_{x}_{y}.png"
+    image = pygame.image.load(filename).convert()
+    tile_surfaces[key] = image
+    return image
+
+def draw_tiles(start, size, screen, zoom=14):
+
+
+    xcount = (size[0] // 256) + 2
+    ycount = (size[1] // 256) + 2
+    tile_range = basemap.get_count_tiles(start, xcount, ycount, zoom)
+
+    #print(tile_range)
+
+    startoffset = basemap.get_offset(zoom, *start)
+    startoffset = (0-startoffset[1], 0-startoffset[0])
+    #startoffset = (0,0)
+    #start_coords = basemap.get_tile_cords(13, *start)
+    for i, xtile in enumerate(range(*tile_range[0])):
+        for j, ytile in enumerate(range(*tile_range[1])):
+
+            image = get_images_cache(zoom, xtile, ytile)
+            offset = (startoffset[0] + (i * 256), startoffset[1] + (j * 256))
+            screen.blit(image, offset)
+
 if __name__ == "__main__":
     pygame.display.set_caption("Intercity Rail Game")
+    screen.fill((255, 255, 255))
+    draw_tiles(startcorner, pygame.display.get_surface().get_size(), screen, zoom=zoom_factor)
     pygame.display.flip()
+
     while True:
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print(f"{cache_hits=}, {cache_misses=}, {100 * (cache_hits/(cache_hits+cache_misses))}%")
                 pygame.quit()
                 sys.exit()
+
+            elif event.type == pygame.VIDEORESIZE or event.type == pygame.VIDEOEXPOSE:
+                draw_tiles(startcorner, pygame.display.get_surface().get_size(), screen, zoom=zoom_factor)
+
+            elif event.type == pygame.MOUSEWHEEL:
+                screen.fill((255, 255, 255))
+                zoom_factor += event.y
+                move_factor = 40 / (2 ** zoom_factor)
+                draw_tiles(startcorner, pygame.display.get_surface().get_size(), screen, zoom=zoom_factor)
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    startcorner[0] += move_factor
+                    draw_tiles(startcorner, pygame.display.get_surface().get_size(), screen, zoom=zoom_factor)
+
+                elif event.key == pygame.K_DOWN:
+                    startcorner[0] -= move_factor
+                    draw_tiles(startcorner, pygame.display.get_surface().get_size(), screen, zoom=zoom_factor)
+
+                elif event.key == pygame.K_LEFT:
+                    startcorner[1] -= move_factor
+                    draw_tiles(startcorner, pygame.display.get_surface().get_size(), screen, zoom=zoom_factor)
+
+                elif event.key == pygame.K_RIGHT:
+                    startcorner[1] += move_factor
+                    draw_tiles(startcorner, pygame.display.get_surface().get_size(), screen, zoom=zoom_factor)
